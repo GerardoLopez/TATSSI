4c96d9adf0cda8c266e8e5a0adacf343ca66bb16
gerardo.lopez <gerardo.lopezsaldana@assimila.eu>
2019-02-06 14:11:38 +0000

    Updated notebook

--- TATSSI/input_output/translate.py
+++ TATSSI/input_output/translate.py
@@ -1,6 +1,5 @@
 
 import os
-import sys
 import gdal
 from .helpers import Constants
 
@@ -43,8 +42,35 @@ class translate():
         return formats
 
     @staticmethod
+    def has_subdatasets(source_img):
+        """
+        Check if file has subdatasets
+        :input: source_img Path of the file to open
+        :return: True if source_img has subdatasets
+        """
+        d = gdal.Open(source_img)
+        if len(d.GetSubDatasets()) == 0:
+            # No SubDatasets
+            return False
+        else:
+            # Has SubDatasets
+            return True
+
+    @staticmethod
+    def get_subdatasets(source_img):
+        """
+        Get subdatasets for a hierarchical data format
+        """
+        d = gdal.Open(source_img)
+        sds = d.GetSubDatasets()
+        
+        return sds
+
+    @staticmethod
     def _check_source_img(source_img):
-        # Check if source_img can be opened by GDAL
+        """
+        Check if source_img can be opened by GDAL
+        """
         gdal.UseExceptions()
         try:
             d = gdal.Open(source_img)
@@ -61,22 +87,34 @@ class translate():
         """
         LOG.info("Converting file %s..." % self.source_img)
 
+        # Source dataset
         src_dataset = gdal.Open(self.source_img)
+
+        # Set output format
         driver = gdal.GetDriverByName(self.output_format)
+        # Do translation
         dst_dataset = driver.CreateCopy(self.target_img,
                                         src_dataset, 0)
 
-        # Make sure any file handles are flushed to ensure file
-        # is written and closed
+        # Flush dataset
         dst_dataset = None
 
         LOG.info("File %s saved" % self.target_img)
 
 if __name__ == '__main__':
-    source_img = 'data/MOD13A2.A2018145.h09v07.006.2018162000027.hdf'
+    dataDir = '/home/glopez/Projects/TATSSI/TATSSI/input_output/data'
+    source_img = 'MOD13A2.A2018145.h09v07.006.2018162000027.hdf'
+    source_img = os.path.join(dataDir, source_img)
     source_img = 'HDF4_EOS:EOS_GRID:"%s":MODIS_Grid_16DAY_1km_VI:1 km 16 days EVI' % source_img
     
-    target_img = 'data/MOD13A2.A2018145.h09v07.006.1km_16_days_EVI.tif'
+    target_img = 'MOD13A2.A2018145.h09v07.006.1km_16_days_EVI.tif'
+    target_img = os.path.join(dataDir, target_img)
 
+    # HDF to GeoTiff (default output format)
     translate(source_img, target_img)
+
+    # HDF to ENVI (img)
+    target_img = 'MOD13A2.A2018145.h09v07.006.1km_16_days_EVI.img'
+    target_img = os.path.join(dataDir, target_img)
+    translate(source_img, target_img, output_format = 'ENVI')
 
--- TATSSI/notebooks/helpers/helpers.py
+++ TATSSI/notebooks/helpers/helpers.py
@@ -1,16 +1,174 @@
 
+import sys
+sys.path.append ("/home/glopez/Projects/TATSSI")
+from TATSSI.input_output.translate import translate
+
+from IPython.display import clear_output
+from IPython.display import display
+from ipywidgets import interact, interactive, fixed, interact_manual
+
+from ipywidgets import Layout
+from ipywidgets import Button, HBox, VBox
+
+import ipywidgets as widgets
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 
-def OpenFileDialog():
-    """
-    Creates a Open File dialog window
-    :return: Full path of selected file
-    """
-    app = QtWidgets.QApplication([dir])
-    fname = QFileDialog.getOpenFileName(None, 
-                                        "Select a file...",
-                                        '.',
-                                        filter="All files (*)")
+import pandas as pd
 
-    return str(fname[0])
+class ImportExport():
+
+    def __init__(self):
+        """
+        Class to handle Input/Output operations in within the notebook
+        """
+        self.input_button = widgets.Button(
+            description = 'Select input file',
+            tooltip = 'Select file or dataset to be imported.'
+        )
+
+        self.input = None
+        self.translate_button = None
+        self.output_button = None
+        self.output = None
+        self.format = None
+        self.import_export()
+
+    def OpenFileDialog(self, dialog_type = 'open',
+                       data_format = 'GeoTiff',
+                       extension = 'tif'):
+        """
+        Creates a Open File dialog window
+        :return: Full path of selected file
+        """
+        app = QtWidgets.QApplication([dir])
+        if dialog_type == 'open':
+            fname = QFileDialog.getOpenFileName(None, 
+                        "Select a file...", '.',
+                        filter="All files (*)")
+
+        elif dialog_type == 'save':
+            # Get format and extension
+            data_format = self.format.value.split('|')[0].strip()
+            extension = self.format.value.split('|')[1].strip()
+
+            fname = QFileDialog.getSaveFileName(None,
+                        "Save file as...", '.',
+                        filter="%s Files (*.%s)" % \
+                                (data_format, extension))
+
+        return str(fname[0])
+
+    def on_output_button_clicked(self, b):
+        """
+        Based on user file selection displays the output file
+        """
+        print(type(self.output))
+        target_img = self.OpenFileDialog('save')
+        if self.output is None:
+            # Show input file in a text file
+            self.output = widgets.Text(
+                              value = target_img,
+                              placeholder = "File to be exported",
+                              description = "Output file",
+                              layout = Layout(width='100%'))
+
+            display(self.output)
+        else:
+            self.output.value = target_img
+
+        if self.translate_button is None:
+            # Create translate button
+            self.translate_button = widgets.Button(
+                description = 'Translate',
+                tooltip = 'Translate input file into output file'
+            )
+
+            self.output_button.on_click(self.on_translate_button_clicked)
+            display(self.translate_button)
+
+    def on_translate_button_clicked(self, b):
+        pass
+
+    def on_input_button_clicked(self, b):
+        """
+        Based on user file selection displays either the input file
+        or the SubDatasets of the selected file
+        """
+        self.clear_cell()
+        display(self.input_button)
+
+        source_img = self.OpenFileDialog('open')
+        if translate.has_subdatasets(source_img):
+            # Get SubDatasets
+            sds = translate.get_subdatasets(source_img)
+            sds_df = pd.DataFrame(list(sds))
+            sds_df.columns = ['SubDataset', 'Resolution']
+
+            # Put SubDatasets in a DropDown list
+            self.input = widgets.Dropdown(
+                             options = list(sds_df['SubDataset']),
+                             value = sds_df['SubDataset'][0],
+                             description = "SubDataset",
+                             layout=Layout(width='100%'))
+        else:
+            # Show input file in a text file
+            self.input = widgets.Text(
+                              value = source_img,
+                              placeholder = "File to be imported",
+                              description = "Input file",
+                              layout = Layout(width='100%'))
+
+        display(self.input)
+
+        # Display output formats
+        formats = translate.get_formats()
+
+        options = list(formats.long_name + " | " + 
+                       formats.short_name + " | " +
+                       formats.extension)
+
+        self.format = widgets.Dropdown(
+                          options = options,
+                          value = options[1],
+                          description = "Output format",
+                          tooltip = "Long name | GDAL driver | extension",
+                          layout=Layout(width='100%'))
+
+        display(self.format)
+
+        # Create output button
+        self.output_button = widgets.Button(
+            description = 'Select output file',
+            tooltip = 'Select output file name and location'
+        )
+
+        self.output_button.on_click(self.on_output_button_clicked)
+        display(self.output_button)
+
+    def import_export(self):
+        """
+        Import and exports file to different GDAL formats
+        """
+        self.input_button.on_click(self.on_input_button_clicked)
+        display(self.input_button)
+
+    def clear_cell(self):
+        clear_output()
+
+        if self.output_button is not None:
+            self.output_button.close()
+
+        if self.translate_button is not None:
+            self.translate_button.close()
+
+        if self.input is not None:
+            self.input.close()
+
+        if self.output is not None:
+            self.output.close()
+
+        if self.format is not None:
+            self.format.close()
+
