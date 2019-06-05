3412f8a043be216363c40b4c5d528a166ac475c5
gerardo.lopez <gerardo.lopezsaldana@assimila.eu>
2019-06-05 23:10:13 +0100

    QA analytics notebook added

--- TATSSI/notebooks/helpers/plot_time_series.py
+++ TATSSI/notebooks/helpers/plot_time_series.py
@@ -47,11 +47,11 @@ class PlotTimeSeries():
     def __init__(self):
         self.fig = plt.figure(figsize=(9.0, 9.0))
 
-        # Data plot
-        self.data_p = plt.subplot2grid((2, 2), (0, 0), colspan=1)
-        # QA plot
-        self.qa_p = plt.subplot2grid((2, 2), (0, 1), colspan=1,
-                                   sharey=self.data_p)
+        # Left plot
+        self.left_p = plt.subplot2grid((2, 2), (0, 0), colspan=1)
+        # Right plot
+        self.right_p = plt.subplot2grid((2, 2), (0, 1), colspan=1,
+                                   sharey=self.left_p)
         # Time series plot
         self.ts_p = plt.subplot2grid((2, 2), (1, 0), colspan=2)
 
@@ -61,35 +61,45 @@ class PlotTimeSeries():
 
         self.ds = None
 
-    def plot(self, ds, qa):
+    def plot(self, left_ds, right_ds, is_qa=False):
         """
         Plot a variable and time series
-        :param fname: Full path file name of time series to plot
+        :param left_ds: xarray to plot on the left panel
+        :param right_ds: xarray to plot on the right panel
         """
-        # Open dataset
-        self.ds = ds
+        self.left_ds = left_ds
         # Create plot
-        self.ds[0].plot(cmap='Greys_r', ax=self.data_p,
-                        add_colorbar=False)
+        self.left_ds[0].plot(cmap='Greys_r', ax=self.left_p,
+                             add_colorbar=False)
+
         # Turn off axis
-        self.data_p.axis('off')
-        self.data_p.set_aspect('equal')
+        self.left_p.axis('off')
+        self.left_p.set_aspect('equal')
 
         # Connect the canvas with the event
         cid = self.fig.canvas.mpl_connect('button_press_event',
                                           self.on_click)
 
         # Plot the centroid
-        _layers, _rows, _cols = self.ds.shape
+        _layers, _rows, _cols = self.left_ds.shape
         # Get y-axis max and min
         #y_min, y_max = self.ds.data.min(), self.ds.data.max()
 
-        plot_sd = self.ds.sel(longitude = int(_cols / 2),
-                              latitude = int(_rows / 2),
-                              method='nearest')
+        plot_sd = self.left_ds.sel(longitude = int(_cols / 2),
+                                   latitude = int(_rows / 2),
+                                   method='nearest')
 
         plot_sd.plot(ax = self.ts_p)
-        #self.bx.set_ylim([y_min, y_max])
+
+        # Right panel
+        self.right_ds = right_ds
+        # Create plot
+        self.right_ds[0].plot(cmap='Greys_r', ax=self.right_p,
+                              add_colorbar=False)
+
+        # Turn off axis
+        self.right_p.axis('off')
+        self.right_p.set_aspect('equal')
 
         plt.tight_layout()
         plt.show()
@@ -101,10 +111,15 @@ class PlotTimeSeries():
         # Clear subplot
         self.ts_p.clear()
 
-        plot_sd = self.ds.sel(longitude=event.xdata,
-                              latitude=event.ydata,
-                              method='nearest')
+        left_plot_sd = self.left_ds.sel(longitude=event.xdata,
+                                        latitude=event.ydata,
+                                        method='nearest')
 
-        plot_sd.plot(ax = self.ts_p)
+        right_plot_sd = self.left_ds.sel(longitude=event.xdata,
+                                         latitude=event.ydata,
+                                         method='nearest')
+
+        left_plot_sd.plot(ax = self.ts_p, color='blue', linestyle = '--')
+        right_plot_sd.plot(ax = self.ts_p, color='black', marker='o')
         # Redraw plot
         plt.draw()
--- TATSSI/notebooks/helpers/qa_analytics.py
+++ TATSSI/notebooks/helpers/qa_analytics.py
@@ -14,8 +14,9 @@ from TATSSI.qa.EOS.catalogue import Catalogue
 # Widgets
 import ipywidgets as widgets
 from ipywidgets import Layout
-from ipywidgets import Button, HBox, VBox
-from ipywidgets import interact, interactive, fixed, interact_manual
+from ipywidgets import Select, SelectMultiple
+from ipywidgets import Button, HBox, VBox, HTML, IntProgress
+#from ipywidgets import interact, interactive, fixed, interact_manual
 
 from beakerx import TableDisplay
 
@@ -26,6 +27,8 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 
 import json
+import collections
+from itertools import groupby as i_groupby
 import gdal, ogr
 import pandas as pd
 import xarray as xr
@@ -38,21 +41,28 @@ import matplotlib
 matplotlib.use('nbagg')
 import matplotlib.pyplot as plt
 
-class QAnalytics():
+class Analytics():
     """
-    Class to provide QA analysis tools
+    Class to provide QA analytics
     """
-    def __init__(self, product, version):
-        # Create TATSSI catalogue object
-        self.catalogue = Catalogue()
+    def __init__(self, source_dir, product, version):
 
-        # Time series object
-        # set on __load_time_series
-        self.ts = None
+        # Check input parameters
+        if os.path.exists(source_dir) is True:
+            self.source_dir = source_dir
+        else:
+            print(f"{source_dir} does not exists!")
+            return None
 
-        # All QA definitions
-        # set on __get_qa_defs
-        self.qa_defs = None
+        if isinstance(product, str) and len(product) > 3:
+            self.product = product
+        else:
+            return None
+
+        if isinstance(version, str) and len(version) == 3:
+            self.version = version
+        else:
+            return None
 
         # QA definition to analise
         # set on qa_ui
@@ -62,14 +72,37 @@ class QAnalytics():
         # set on qa_ui
         self.user_qa_selection = None
 
-        # Display QA User Interface
-        self.__qa_ui()
+        # Mask based on user_qa_selection
+        # set on __analytics
+        self.mask = None
 
-    def __qa_ui():
+        # Percentage of data available after masking
+        # set on __analytics
+        self.pct_data_available = None
+
+        # Max gap length
+        # set on get_max_gap_length
+        self.max_gap_length = None
+
+        # Create TATSSI catalogue object
+        self.catalogue = Catalogue()
+
+        # Time series object
+        self.ts = self.__load_time_series()
+
+        # All QA definitions
+        self.qa_defs = self.__get_qa_defs()
+
+    def ui(self):
         """
         QA user interface
         """
-        self.qa_def = self.qa_defs[1]
+        # Clear cell
+        self.__clear_cell()
+
+        if self.qa_def is None:
+            # Use the first or unique QA
+            self.qa_def = self.qa_defs[0]
 
         qa_flags = self.qa_def.Name.unique()
         qa_layer = self.qa_def.QualityLayer.unique()
@@ -78,9 +111,10 @@ class QAnalytics():
             value = f"<b>{qa_layer[0]}</b>",
             description='QA layer:'
         )
-        display(qa_layer_header)
 
-        self.user_qa_selection = dict((element, '') for element in qa_flags)
+        self.user_qa_selection = collections.OrderedDict(
+                (element, '') for element in qa_flags)
+
         # Fill default selection
         for i, selection in enumerate(self.user_qa_selection):
             self.user_qa_selection[selection] = tuple(
@@ -93,22 +127,22 @@ class QAnalytics():
             rows=len(qa_flags),
             description='QA Parameter name:',
             style = {'description_width': 'initial'},
-            layout={'width': '350px'},
+            layout={'width': '400px'},
             disabled=False
         )
 
         def on_qa_flag_change(change):
             if change['type'] == 'change' and change['name'] == 'value':
-            qa_flag_value = change.owner.value
+                qa_flag_value = change.owner.value
         
-            # Get user selection before changing qa description
-            tmp_selection = self.user_qa_selection[qa_flag_value]
+                # Get user selection before changing qa description
+                tmp_selection = self.user_qa_selection[qa_flag_value]
 
-            _options = self.qa_def[self.qa_def.Name == qa_flag_value].Description.tolist()
-            qa_description.options = _options
+                _options = self.qa_def[self.qa_def.Name == qa_flag_value].Description.tolist()
+                qa_description.options = _options
         
-            qa_description.rows = len(_options)
-            qa_description.value = tmp_selection
+                qa_description.rows = len(_options)
+                qa_description.value = tmp_selection
     
         qa_flag.observe(on_qa_flag_change)
 
@@ -128,7 +162,7 @@ class QAnalytics():
 
         def on_qa_description_change(change):
             if change['type'] == 'change' and change['name'] == 'value':
-            self.user_qa_selection[qa_flag.value] = qa_description.value
+                self.user_qa_selection[qa_flag.value] = qa_description.value
 
         qa_description.observe(on_qa_description_change)
 
@@ -169,39 +203,54 @@ class QAnalytics():
 
         left_box = VBox([qa_flag])
         right_box = VBox([qa_description])
-        HBox([qa_flag, right_box, select_all, select_default],
-             layout={'height': '350px',
-                     'width' : '99%'}
+        #_HBox = HBox([qa_flag, right_box, select_all, select_default],
+        _HBox_qa = HBox([left_box, right_box],
+                        layout={'height': '300px',
+                                'width' : '99%'}
         )
 
-    def __load_time_series(source_dir, product):
+        analytics = Button(
+            description = 'QA analytics',
+            layout={'width': '30%'}
+        )
+        analytics.on_click(self.__analytics)
+
+        # Display QA HBox
+        display(qa_layer_header, _HBox_qa)
+        
+        _HBox_buttons = HBox([select_all, select_default, analytics])
+        display(_HBox_buttons)
+
+    def __load_time_series(self):
         """
         Loads existing time series using the TATSSI
         time series Generator class
-        :param source_dir: root directory where GeoTiff's and VRTs
+        :attr self.source_dir: root directory where GeoTiff's and VRTs
                            are stored
-        :param product: product and version, e.g. 'MOD13A2.006'
+        :attr self.product: product name, e.g. 'MOD13A2'
+        :atte self.version: version of the product '006'
         :return time series TATSSI object
         """
         # Create time series generator object
-        tsg = Generator(source_dir = DataDir,
-                        product = product)
+        product_and_version = f'{self.product}.{self.version}'
+        tsg = Generator(source_dir=self.source_dir,
+                        product=product_and_version)
 
         # Load time series
-        self.ts = tsg.load_time_series()
+        return tsg.load_time_series()
 
-        # User's QA selection
-        self.user_qa_selection
-
-    def __get_qa_defs(product, version):
+    def __get_qa_defs(self):
         """
         Get QA definitions for a particular product and version and
         changes the 'Value' field from decimal to binary. This is 
         neccesary because the decoded QA GeoTifts have this binary
         values stored.
+        :attr self.product: product name, e.g. 'MOD13A2'
+        :atte self.version: version of the product '006'
+        :return QA definitions DataFrame
         """
-        qa_defs = self.catalogue.get_qa_definition(product=product,
-                                                   version=version)
+        qa_defs = self.catalogue.get_qa_definition(product=self.product,
+                                                   version=self.version)
         # Convert to binary
         for qa_def in qa_defs:
             binary_vals_list = []
@@ -211,6 +260,124 @@ class QAnalytics():
             # Update the 'Value' field
             qa_def['Value'] = binary_vals_list
 
-        self.qa_defs = qa_defs
+        return qa_defs
 
+    def __analytics(self, b):
+        """
+        Uses the self.user_qa_selection OrderedDictionary to extract
+        the corresponding QA values and create a mask of dimensions:
+            (number of qa layers, time steps, cols(lat), rows(lon))
+        Additionally computes the temporal mask and the max gap length
+        """
+        progress_bar = IntProgress(
+                value=0,
+                min=0,
+                max=len(self.user_qa_selection),
+                step=1,
+                description='',
+                bar_style='', # 'success', 'info', 'warning', 'danger' or ''
+                orientation='horizontal',
+                style = {'description_width': 'initial'},
+                layout={'width': '50%'}
+        )
+        display(progress_bar)
 
+        n_qa_layers = len(self.user_qa_selection)
+
+        # Get the name of the first data var to extract its shape
+        for k, v in self.ts.data.data_vars.items():
+            break
+
+        # Create mask xarray
+        _time, _latitude, _longitude = self.ts.data.data_vars[k].shape
+        mask = np.zeros((n_qa_layers, _time, _latitude, _longitude),
+                        np.int8)
+
+        qa_layer = self.qa_def.QualityLayer.unique()
+
+        # QA layer user to create mask
+        _qa_layer = getattr(self.ts.qa, f"qa{qa_layer[0]}")
+
+        for i, user_qa in enumerate(self.user_qa_selection):
+            progress_bar.value = i
+
+            user_qa_fieldname = user_qa.replace(" ", "_").replace("/", "_")
+            progress_bar.description = "Masking by user QA selection"
+
+            for j, qa_value in enumerate(self.user_qa_selection[user_qa]):
+                qa_value_field_name = qa_value.replace(" ", "_")
+
+                qa_flag_val = self.qa_def[(self.qa_def.Name == user_qa) & 
+                        (self.qa_def.Description == qa_value)].Value.iloc[0]
+
+                if j == 0 :
+                    mask[i] = (_qa_layer[user_qa_fieldname] == qa_flag_val)
+                else:
+                    mask[i] = np.logical_or(
+                                  mask[i], _qa_layer[user_qa_fieldname] == qa_flag_val)
+
+        # Remove progress bar
+        progress_bar.close()
+        del progress_bar
+
+        self.__temp_mask = mask
+        mask = xr.DataArray(np.all(self.__temp_mask, axis=0),
+                            dims=('time', 'latitude', 'longitude'))
+
+        mask.time.data = v.time.data
+        mask.latitude.data = v.latitude.data
+        mask.longitude.data = v.longitude.data
+        mask.attrs = v.attrs
+
+        self.mask = mask
+
+        # Create the percentage of data available mask
+        # Get the per-pixel per-time step binary mask
+        pct_data_available = (self.mask.sum(axis=0) * 100.0) / _time
+        self.pct_data_available = pct_data_available
+
+        # Using the computed mask get the max gap length
+        self.__get_max_gap_length()
+
+    def __get_max_gap_length(self):
+        """
+        Compute the max gep length of a masked time series
+        """
+        # TODO
+        # This function should be paralelised! 
+
+        bands, rows, cols = self.mask.shape
+        #max_gap_length = np.zeros((rows,cols), np.int16)
+        max_gap_length = xr.zeros_like(self.mask[0])
+
+        progress_bar = IntProgress(
+                value=0,
+                min=0,
+                max=10,
+                step=1,
+                description='Computing max gap length...',
+                bar_style='', # 'success', 'info', 'warning', 'danger' or ''
+                orientation='horizontal',
+                style = {'description_width': 'initial'},
+                layout={'width': '50%'}
+        )
+        display(progress_bar)
+
+        for i in range(rows):
+            progress_bar.value = int((i*10.)/rows)
+            for j in range(cols):
+                for key, group in i_groupby(self.mask.data[:,i,j]):
+                    if key == False:
+                        _gap_lenght = len(list(group))
+                        if _gap_lenght > 0 and _gap_lenght > max_gap_length.data[i,j]:
+                            max_gap_length.data[i,j] = _gap_lenght
+
+        # Remove progress bar
+        progress_bar.close()
+        del progress_bar
+
+        self.max_gap_length = max_gap_length
+
+    def __clear_cell(self):
+        """ Clear cell """
+        clear_output()
